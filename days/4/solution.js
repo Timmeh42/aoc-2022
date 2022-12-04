c59ecc6056350f84aceebc93835a5a29b45e0d16
timmeh42 <tmeier42+github@gmail.com>
2022-12-04 09:39:20 +0200

    the silly high speed version

--- days/4/solution.js
+++ days/4/solution.js
@@ -1,19 +1,36 @@
 module.exports = function (input) {
-    input = input.trim();
-    const elfPairs = input.split('\n').map(line => line.match(/\d+/g).map(n => parseInt(n)));
     let part1 = 0;
     let part2 = 0;
-    for (let pair of elfPairs) {
-        if (
-            (pair[0] <= pair[2] && pair[1] >= pair[3])
-          || (pair[0] >= pair[2] && pair[1] <= pair[3])
-        ) {
-            part1++;
-        }
-        if (
-            (pair[0] <= pair[3] && pair[1] >= pair[2])
-        ) {
-            part2++;
+    let currentNumber = 0;
+    let last4 = new Uint8Array(4);
+    let lastIndex = 0;
+    let readingNumber = true;
+    for (let s = 0; s < input.length; s++) {
+        const char = input.charCodeAt(s);
+        if (char >= 48 && char <=57) {
+            readingNumber = true;
+            currentNumber = currentNumber * 10 + char - 48;
+        } else {
+            if (readingNumber) {
+                readingNumber = false;
+                last4[lastIndex] = currentNumber;
+                currentNumber = 0;
+                lastIndex += 1;
+                if (lastIndex === 4) {
+                    lastIndex = 0;
+                    if (
+                        (last4[0] <= last4[2] && last4[1] >= last4[3])
+                      || (last4[0] >= last4[2] && last4[1] <= last4[3])
+                    ) {
+                        part1++;
+                    }
+                    if (
+                        (last4[0] <= last4[3] && last4[1] >= last4[2])
+                    ) {
+                        part2++;
+                    }
+                }
+            }
         }
     }
 
